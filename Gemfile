0601cce21e5dffbf7976df142fb026eba26afd7a
Bernadette <bbatanes17@yahoo.com>
2018-08-28 03:21:02 +0000

    Added Authenticatin with Devise

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.3.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
+
+#adding authentication with devise
+gem 'devise'
+
 # Use sqlite3 as the database for Active Record
 group :development do
   gem 'sqlite3'
@@ -66,3 +70,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  
+  
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -58,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  #Default url options
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
